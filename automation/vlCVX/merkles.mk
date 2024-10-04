ff66815043c49eaba5b9e341904af5a30f7179f4
Laouni <99427751+Lao0ni@users.noreply.github.com>
2024-10-05 00:50:37 +0200

    ✨ feat: adding make for merkle generation

---
+++ automation/vlCVX/merkles.mk
@@ -0,0 +1,34 @@
+include automation/setup/dotenv.mk
+include automation/setup/node.mk
+
+.PHONY: all setup install-deps run-merkles clean
+
+# Define the default target
+.DEFAULT_GOAL := all
+
+all: setup install-deps run-merkles
+
+setup: setup-node
+
+install-deps:
+	@echo "Installing dependencies..."
+	@$(PNPM) install
+	@$(PNPM) add -D tsx
+
+run-merkles: setup install-deps
+	@echo "Running merkles generation..."
+	@$(PNPM) tsx script/vlCVX/3_merkles.ts
+
+commit-and-push:
+	@echo "Committing and pushing changes..."
+	@git add .
+	@git config --global user.name 'GitHub Action'
+	@git config --global user.email 'action@github.com'
+	@git add bounties-reports
+	@git commit -m "Add vlCVX merkles" || true
+	@git push
+
+clean:
+	@echo "Cleaning up local files..."
+	@rm -rf node_modules
+	@$(MAKE) -f automation/setup/node.mk clean-node
