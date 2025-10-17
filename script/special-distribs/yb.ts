e5902ede720bceb07e21200b88f7ae51bea0be5f
Marsot Pierre <pierre.mmarsot@gmail.com>
2025-10-17 13:48:00 +0200

    yb distribution

--- script/special-distribs/yb.ts
+++ script/special-distribs/yb.ts
@@ -4,14 +4,15 @@ import { createCombineDistribution } from '../utils/merkle/merkle';
 import { generateMerkleTree } from '../vlCVX/utils';
 import { MerkleData } from '../interfaces/MerkleData';
 import { Distribution } from '../interfaces/Distribution';
+import axios from 'axios';
 
 const main = async () => {
     // Load YB merkle
     const ybMerkle: MerkleData = JSON.parse(fs.readFileSync(path.resolve(__dirname, "yb_merkle.json"), { encoding: 'utf-8' }));
 
     // Load current extra reward merkle
-    const pathExtraMerkle = path.resolve(__dirname, "..", "..", "data", "extra_merkle", "merkle.json")
-    const extraRewardMerkle: MerkleData = JSON.parse(fs.readFileSync(pathExtraMerkle, { encoding: 'utf-8' }));
+    const {data} = await axios.get("https://raw.githubusercontent.com/stake-dao/bounties-report/refs/heads/main/data/extra_merkle/merkle.json");
+    const extraRewardMerkle = data as MerkleData;
 
     // Convert userRewards to Distribution format
     const currentDistribution: Distribution = {};
@@ -33,6 +34,9 @@ const main = async () => {
 
     // Generate merkle tree using shared utility
     const merkleData = generateMerkleTree(combinedDistribution);
+
+    // Write merkle file
+    const pathExtraMerkle = path.resolve(__dirname, "..", "..", "data", "extra_merkle", "merkle.json")
     fs.writeFileSync(pathExtraMerkle, JSON.stringify(merkleData, null, 2));
 }
 
